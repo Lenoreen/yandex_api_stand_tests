8be020c686b454add3fcd9da73467c9a9f9ac2ed
elena p <elenapolotova@gmail.com>
2024-02-25 19:33:39 +0500

    My first commit

---
+++ check.py
@@ -0,0 +1,6 @@
+def count_by(x, n):
+    list = []
+    for i in range(1, n + 1):
+        list.append(i * x)
+    return list
+count_by(2,7)
---
+++ configuration.py
@@ -0,0 +1,11 @@
+URL_SERVICE = 'https://d3e0b2c4-4b6d-44c5-9140-ebc228c3f7bb.serverhub.praktikum-services.ru'
+
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
